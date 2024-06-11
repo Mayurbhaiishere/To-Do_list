a2ee7988f8c3754b2a8bd396acb05a77702f0a36
Mayurbhaiishere <sunilsupaduahirrao65197@gmail.com>
2024-06-12 00:03:32 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,49 @@
+let toggle1 = 0;
+
+document.getElementById("ok").addEventListener("click", function () {
+  const taskValue = document.getElementById("task").value.trim();
+  if (taskValue !== "") {
+    const taskWrapper = document.createElement("div");
+
+    taskWrapper.className = "task-wrapper";
+
+    const toggle = document.createElement("radio");
+    toggle.className = "toggle-before";
+    toggle.textContent = "";
+
+    const emptyItem = document.createElement("div");
+    emptyItem.className = "empty-div";
+    emptyItem.textContent = "";
+
+    const taskItem = document.createElement("div");
+    taskItem.className = "task-item-before";
+    taskItem.textContent = taskValue;
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.className = "delete";
+    deleteBtn.textContent = "x";
+    deleteBtn.addEventListener("click", function () {
+      taskWrapper.remove();
+      emptyItem.remove();
+    });
+
+    toggle.addEventListener("click", function () {
+      if (toggle1 == 0) {
+        toggle.className = "toggle-after";
+        toggle1 = 1;
+        taskItem.className = "task-item-after";
+      } else {
+        toggle.className = "toggle-before";
+        toggle1 = 0;
+        taskItem.className = "task-item-before";
+      }
+    });
+
+    document.getElementById("main").appendChild(emptyItem);
+    taskWrapper.appendChild(taskItem);
+    taskWrapper.appendChild(toggle);
+    taskWrapper.appendChild(deleteBtn);
+    document.getElementById("main").appendChild(taskWrapper);
+    document.getElementById("task").value = "";
+  }
+});
